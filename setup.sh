1bba306c5cabd9ffcc3a88fc0aeb8b0b0f314a52
Aleksander Kaminski <aleksander.kaminski@protonmail.com>
2024-12-18 01:29:05 +0000

    Added setup

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+git remote remove origin
+git checkout --orphan temp_branch
+
+sed -i "s/PROJECT_NAME/$(basename $(pwd))" Cargo.toml
+
+git add Cargo.toml
+git add .envrc
+git add .gitignore
+git add flake.nix
+git commit -m "Init"
+git branch -d master
+git branch -m master
+
+rm setup.sh
+
